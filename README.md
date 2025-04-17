6b5954eed8926edb9cb991650b7d1a5ae71ba92d
kamplianitis <k.amplianitis@sphynx.ch>
2025-04-17 12:06:08 +0300

    initial commit of ml project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Premier League ML Project
+
+A project that was made during the MLDS master program in Techincal University of Crete.
---
+++ main.py
@@ -0,0 +1,5 @@
+import kagglehub
+
+path = kagglehub.dataset_download("ajaxianazarenka/premier-league")
+
+print("Path to dataset files:", path)
