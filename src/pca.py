547f23d56c62309265a96dca88f8b09ffd2628a0
kamplianitis <k.amplianitis@sphynx.ch>
2025-06-01 10:58:14 +0300

    chore(pca): remove extra packages

--- src/pca.py
+++ src/pca.py
@@ -1,7 +1,5 @@
 import pandas as pd
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
 from typing import Literal
 
 
